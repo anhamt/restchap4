647d65ad31ace3948ad9b7ffaeaf3ba555d4762e
anhamt <75478354+anhamt@users.noreply.github.com>
2021-04-15 16:19:14 +0800

    Update user-detail.component.ts

--- src/app/components/user/user-detail.component.ts
+++ src/app/components/user/user-detail.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { ReqresService } from '../../services/reqres.service';
-import { User } from '../../user';
 
 @Component({
   selector: 'app-user-detail',
@@ -9,25 +8,16 @@ import { User } from '../../user';
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
-  user: User;
+  user: any;
 
-  constructor(
-    private activatedRoute: ActivatedRoute,
-    private reqresService: ReqresService,
-    private router: Router
-  ) {
-    this.activatedRoute.params.subscribe((params) => {
-      reqresService.getUser(params ['id'])
-        .subscribe((res: User) => this.user = res);
+  constructor( private activatedRoute: ActivatedRoute, private reqresService: ReqresService ) {
+    this.activatedRoute.params.subscribe( (params) => {
+      this.user = reqresService.getUser( params['id'] )
+        .subscribe( (res: any) => this.user = res.data);
     });
   }
 
-  ngOnInit(): void {
-  }
-
-  save(): void {
-    this.reqresService.updateUser(this.user)
-      .subscribe(() => this.router.navigate( ['users'] ));
+  ngOnInit() {
   }
 
 }
