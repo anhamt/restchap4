8a889edcb0698de2d26d91c92046938810c1fe17
anhamt <75478354+anhamt@users.noreply.github.com>
2021-04-15 16:25:05 +0800

    Update reqres.service.ts

--- src/app/services/reqres.service.ts
+++ src/app/services/reqres.service.ts
@@ -1,9 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { User } from '../user';
-
-import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,59 +9,15 @@ import { catchError, map } from 'rxjs/operators';
 export class ReqresService {
   private url = 'api/users';
 
-  httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  };
-
   constructor( private http: HttpClient ) { }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.url)
-      .pipe(
-        catchError(this.handleError<User[]>('getUsers', []))
-      );
+    return this.http.get<User[]>(this.url);
   }
 
   getUser( id: number ): Observable<User> {
     const url = `${this.url}/${id}`;
 
-    return this.http.get<User>(url)
-      .pipe(
-        catchError(this.handleError<User>(`getUser id=${id}`))
-      );
-  }
-
-  updateUser(user: User): any {
-    return this.http.put(this.url, user, this.httpOptions)
-      .pipe(
-        catchError(this.handleError<User>('updateUser'))
-      );
-  }
-
-  addUser( user: User ): Observable<User> {
-    return this.http.post<User>(this.url, user, this.httpOptions)
-      .pipe(
-        catchError(this.handleError<User>('addUser'))
-      );
-  }
-
-  deleteUser( user: User ): Observable<User> {
-    const url = `${this.url}/${user.id}`;
-
-    return this.http.delete<User>(url, this.httpOptions)
-      .pipe(
-        catchError(this.handleError<User>('deleteUser id=${user.id}'))
-      );
-  }
-
-  private handleError<T>(operation = 'operation', result?: T): any {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
+    return this.http.get<User>(url);
   }
 }
